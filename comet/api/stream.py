e441587146a39f9bc19fa311b3b99b63740aaaee
Isaque Victor Duarte De Oliveira <45144815+109isaque10@users.noreply.github.com>
2024-09-18 00:42:50 -0300

    Update stream.py

--- comet/api/stream.py
+++ comet/api/stream.py
@@ -406,14 +406,8 @@ async def stream(request: Request, b64config: str, type: str, id: str):
         }
         for torrent in torrents:
             if "Debrid" not in torrent:
-                logger.info('not debrid')
-                torrents_by_hash = torrent["InfoHash"] 
-                logger.info(torrents_by_hash)
+                torrents_by_hash = {torrent["InfoHash"]: torrent}
         for hash in sorted_ranked_files:  # needed for caching
-            logger.info(str(torrents_by_hash))
-            logger.info(hash)
-            if hash not in torrents_by_hash:
-                continue
             sorted_ranked_files[hash]["data"]["title"] = files[hash]["title"]
             sorted_ranked_files[hash]["data"]["torrent_title"] = torrents_by_hash[hash]["Title"]
             sorted_ranked_files[hash]["data"]["tracker"] = torrents_by_hash[hash]["Tracker"]
